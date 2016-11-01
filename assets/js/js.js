466b2a2ead0d76cda1c3d985de8849e571132fe0
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-11-01 13:43:29 -0400

    fixed JS submit button

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,50 +1,53 @@
-//alert("Hi");
-
 var counter = 0;
 var intervalId;
 var correct = 0;
 var incorrect = 0;
-var pointsCorrect = [];
-var pointIncorrect = [];
-var userClick = '';
-var questionNumber;
 
 $(document).ready(function(){
 	$("#results").hide();
 		$('#questions').addClass('hide');
 	$('#startTrivia').on('click', function(){
 		$('#questions').removeClass('hide');
-		// $("#timer").counter();
+		startTimer();
 	});
 
-	//results will show after pushing submit
 	$("#submit").on('click', function(){
+		//get the number of correct... :checked is what the user checked
+		var numberCorrect = $('input[data-correct="correct"]:checked').length;
+		var numberIncorrect = 10 - numberCorrect;
+		//puts in the html what questions were correct vs incorrect
+		$('#correct').html(numberCorrect);
+		$("#incorrect").html(numberIncorrect);
+		//results will show after pushing submit
 		$("#results").show();
-	});
-
-	//timer starts after clicking start timer
-	$('#startTimer').on('click', function(){
-		intervalId = setInterval(totalTime, 1000);
+		//stops timer
+		pauseTimer();
 	});
 
 	//timer stops after clicking stop timer
 	$("#stopTimer").on('click', function(){
-		
+		pauseTimer();
 	});
 
-
-//get the number of correct... :checked is what the user checked
-var numberCorrect = $('input[data-correct="correct"]:checked').length;
-var numberIncorrect = 6 - numberCorrect;
-
-	//puts in the html what questions were correct vs incorrect
-	$('#correct').html(numberCorrect);
-	$("#incorrect").html(numberIncorrect);
+	$('#reset').on('click', function(){
+        resetTimer();
+    });
 });
-
-
 
 function totalTime() {
 	counter++;
 	$('#timer').html(counter);
+};
+
+function pauseTimer() {
+	this.clearInterval(intervalId);
+};
+
+function startTimer() {
+	intervalId = setInterval(totalTime, 1000);
+};
+
+function resetTimer() {
+	intervalId = window.clearInterval(intervalId);
+	counter = 0;
 };
