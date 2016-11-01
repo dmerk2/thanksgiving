352fd4a1c57db637e3c12e983453cff7d0f1a54d
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-11-01 13:02:51 -0400

    added buttons and set to tasks assigned

--- assets/js/js.js
+++ assets/js/js.js
@@ -2,16 +2,47 @@
 
 var counter = 0;
 var intervalId;
+var correct = 0;
+var incorrect = 0;
+var pointsCorrect = [];
+var pointIncorrect = [];
+var userClick = '';
+var questionNumber;
 
 $(document).ready(function(){
+	$("#results").hide();
 		$('#questions').addClass('hide');
-	$('#start').click(function(){
+	$('#startTrivia').on('click', function(){
 		$('#questions').removeClass('hide');
 		// $("#timer").counter();
 	});
+
+	//results will show after pushing submit
+	$("#submit").on('click', function(){
+		$("#results").show();
+	});
+
+	//timer starts after clicking start timer
+	$('#startTimer').on('click', function(){
+		intervalId = setInterval(totalTime, 1000);
+	});
+
+	//timer stops after clicking stop timer
+	$("#stopTimer").on('click', function(){
+		
+	});
+
+
+//get the number of correct... :checked is what the user checked
+var numberCorrect = $('input[data-correct="correct"]:checked').length;
+var numberIncorrect = 6 - numberCorrect;
+
+	//puts in the html what questions were correct vs incorrect
+	$('#correct').html(numberCorrect);
+	$("#incorrect").html(numberIncorrect);
 });
 
-intervalId = setInterval(totalTime, 1000);
+
 
 function totalTime() {
 	counter++;
