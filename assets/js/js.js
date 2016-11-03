a27ca3714c601377eda2ae6ccadf5a85c6869889
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-11-03 19:07:21 -0400

    adding grade

--- assets/js/js.js
+++ assets/js/js.js
@@ -5,6 +5,8 @@ var correct = 0;
 var incorrect = 0;
 var totalQuestions = 10;
 var timeSpent;
+var grade;
+var percent;
 
 $(document).ready(function(){
 	$("#results").addClass('hide');
@@ -23,10 +25,18 @@ $(document).ready(function(){
 		//puts in the html what questions were correct vs incorrect
 		$('#correct').html(numberCorrect);
 		$("#incorrect").html(numberIncorrect);
+		
+
+		//inserting the grade you recieved into html
+		// grade = "You recieved a " + grade();
+		
+
+		$("#grade").html(grade);
+		//inputing amount of time into html
 		timeSpent = "It took you " + minutes + " minutes and " + seconds + " seconds to complete the Thanksgiving Trivia!"
 		$("#timeTaken").html(timeSpent);
 		//calculating percentage of correct answers
-		$("#totalPercent").html("You got " + percentCorrect(numberCorrect, totalQuestions) + "% on the test!");
+		$("#totalPercent").html("You recieved " + percentCorrect(numberCorrect, totalQuestions) + "% on the quiz!");
 		//results will show after pushing submit
 		$("#results").removeClass('hide');	
 	});
@@ -48,6 +58,20 @@ $(document).ready(function(){
     });
 });
 
+// function grade() {
+// 	if(percent >= 90) {
+// 		grade = "A";
+// 	} else if(percent >= 80 && percent <= 89) {
+// 		grade = "B";
+// 	} else if(percent >= 70 && percent <= 79) {
+// 		grade = "C";
+// 	} else if(percent >= 60 && percent <= 69) {
+// 		grade = "D";
+// 	} else {
+// 		grade = "F";
+// 	};
+// };
+
 function percentCorrect(x, y) {
 	return Math.round((x/y)*100);
 };
@@ -60,8 +84,8 @@ function totalTime() {
 			// if seconds is 60 than reset seconds back to 0
 			if(seconds == 60) {
 				seconds = 0;
-				$("#minutes").html(minutes);
-		};
+					$("#minutes").html(minutes);
+		}
 };
 
 function pauseTimer() {
