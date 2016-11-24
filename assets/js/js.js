1c41455d748d5523d3c858a93356668b8aa966f3
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-11-24 14:49:56 -0500

    adding correct answers button

--- assets/js/js.js
+++ assets/js/js.js
@@ -8,13 +8,12 @@ var timeSpent;
 
 var percent = function percentCorrect(x, y) {
 	var number = Math.round((x/y)*100);
-	// console.log(number);
 	return number;
 };
 
+// ALWAYS GIVES "F" AS GRADE!!!
 var gradeFunction = function() {
 	var gradeResult;
-	// console.log(percent);
 	if(percent >= 90) {
 		gradeResult = "A";
 	} else if((percent >= 80) && (percent <= 89)) {
@@ -25,36 +24,43 @@ var gradeFunction = function() {
 		gradeResult = "D";
 	} else {
 		gradeResult = "F";
-	};
-
+	}
 	return gradeResult;
-};;
+};
 
-$(document).ready(function(){
-	$("#results").addClass('hide');
+	
 	$('#questions').addClass('hide');
-	$('#startTrivia').on('click', function(){
-		$('#questions').removeClass('hide');
-		startTimer();
+	$("#questions2").addClass('hide');
+	$("#correctAnswersDiv").addClass("hide");
+	$("#results").addClass('hide');
+	
+	
+$(document).ready(function(){
+
+	$('#startTrivia').on('click', function(){	
+	startTimer();
+	$('#questions').removeClass('hide');
 	});
 
 	$("#submit").on('click', function(){
 		$(".jumbotron").addClass('hide');
 		$("#questions").addClass('hide');
-		//get the number of correct... :checked is what the user checked
+		// get the number of correct... :checked is what the user checked
 		var numberCorrect = $('input[data-correct="correct"]:checked').length;
 		var numberIncorrect = 10 - numberCorrect;
-		//puts in the html what questions were correct vs incorrect
+		// console.log(numberIncorrect);
+		// console.log(numberCorrect);
+		// puts in the html what questions were correct vs incorrect
 		$('#correct').html(numberCorrect);
 		$("#incorrect").html(numberIncorrect);
 		
 
-		//inserting the grade you recieved into html
+		// inserting the grade you recieved into html
 		var gradeString = "You recieved a " + gradeFunction();
 		$("#grade").text(gradeString);
 
 
-		//inputing amount of time into html
+		// inputing amount of time into html
 		timeSpent = "It took you " + minutes + " minutes and " + seconds + " seconds to complete the Thanksgiving Trivia!"
 		$("#timeTaken").html(timeSpent);
 		//calculating percentage of correct answers
@@ -63,44 +69,38 @@ $(document).ready(function(){
 		$("#results").removeClass('hide');	
 	});
 
-	//timer pauses after clicking pause timer
+	// timer pauses after clicking pause timer
 	$("#pauseGame").on('click', function(){
 		pauseTimer();
-		$("#questions").addClass('hide');
+		$("#questions2").addClass('hide');
 	});
 
-	//timer resets after clicking reset timer
+	// timer resets after clicking reset timer
 	$('#restart').on('click', function(){
         resetGame();        
-        $("#questions").addClass('hide');
+        $("#questions2").addClass('hide');
         $("#results").addClass('hide');
         $(".jumbotron").removeClass('hide');
         //clearing the checked radio buttons
     	$("input:checked").removeAttr("checked");
     });
+		
+    $("#correctAnswersButton").on("click", function(){   	
+    	$("#correctAnswersDiv").removeClass("hide");
+    	$("#questions").addClass('hide');
+    	$("#results").addClass('hide');
+    	$(".jumbotron").addClass('hide');
+    });
+
+    $("#startTrivia2").on("click", function(){
+    	$(".jumbotron2").addClass('hide');
+    	$(".jumbotron").removeClass('hide');
+    	$(".jumbotron2").removeClass('hide');
+    	$("#correctAnswersDiv").removeClass('hide');
+    	$("#questions").addClass('hide');
+	});
 });
 
-
-
-// GRADE RETURNS UNDEFINED
-// switch(grade) {
-// 	case 'A': percent >= 90;
-// 	break;
-
-// 	case 'B': percent >= 80 && percent <= 89;
-// 	break;
-
-// 	case 'C': percent >= 70 && percent <= 79;
-// 	break;
-
-// 	case 'D': percent >= 60 && percent <= 69;
-// 	break;
-
-// 	default: 'F';
-// };
-
-
-// NEED TO RESET MINUTES
 function totalTime() {
 	seconds++;
 	$("#seconds").html(seconds);	
@@ -109,7 +109,6 @@ function totalTime() {
 	// if seconds is 60 than reset seconds back to 0
 	if(seconds == 60) {
 		seconds = 0;
-		
 	}
 	$("#minutes").html(minutes);
 };
